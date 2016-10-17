8fd05cd0ee416adee7e47aeb5c4115945f2510ca
VVShatokhina <vlada6v@yandex.ru>
2016-10-17 20:40:51 +0300

    Add files via upload

---
+++ hw3/1 вариант.py
@@ -0,0 +1,12 @@
+a=int(input('Введите значение a: '))
+b=int(input('Введите значение b: '))
+c=int(input('Введите значение c: '))
+x=c
+if a+b==c:
+    print ('yes')
+elif a+b!=c:
+    print ('no')
+if a*x+b==0:
+    print ('yesyes')
+    
+
