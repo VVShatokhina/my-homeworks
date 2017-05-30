faeb387a12331f4800fabc2a02acce5dc5c1df0b
VVShatokhina <vlada6v@yandex.ru>
2017-05-30 10:43:27 +0300

    Update ex.py

--- экзамен/ex.py
+++ экзамен/ex.py
@@ -4,11 +4,11 @@ with open('zamok.txt', 'r', encoding = 'UTF-8') as f:
     text = f.read()
     words = text.split()
 for word in words:
-    s1 = re.search('[^а-я]\.\s[*А-Я-а-я]+', word)
+    s1 = re.search('^[А-Я]\.\s^[А-Я][^\s\d]+', word)
 for wordd in words:
-    s2 = re.search('[^а-я]\.\s[^а-я]\.\s[*А-Я-а-я]+', word)
+    s2 = re.search('^[А-Я]\.\s^[А-Я]\.\s[А-Я][^\s\d]', word)
 for worddd in words:
-    s3 = re.search('[А-Я-а-я]\s[А-Я-а-я]+', word)
+    s3 = re.search('^[А-Я]\s^[А-Я][^\s\d]', word)
 print('инициал + фамилия:', word)
 print('инициалы + фамилия:', wordd)
 print('имя + фамилия:', worddd)
